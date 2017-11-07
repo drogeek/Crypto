2cbcceaa96ff2513f11cc252d8618da2e76b8250
toor <guiennet.jonathan@gmail.com>
2017-11-07 23:04:31 +0100

    add multbyalpha on galois field

--- GaloisFields.py
+++ GaloisFields.py
@@ -1,3 +1,6 @@
+from operator import xor
+from functools import reduce
+
 def primeInInterval(N):
     primeNbrs = list(range(2,N))
     i=0
@@ -18,4 +21,38 @@ def GFInInterval(N):
     return gf
 
 N=300
-print(len(GFInInterval(N)))
+#print(len(GFInInterval(N)))
+
+def multbyalpha(b,f):
+    result = b
+    h_order_b_position = 0
+    while f>>h_order_b_position != 0:
+        h_order_b_position+=1
+    mask = 2**(h_order_b_position-1)-1
+    overflow = mask&f
+
+    result<<=1
+    if result > mask:
+        result&=mask
+        result^=overflow
+    return result 
+res=1
+for i in range(8):
+    print(res)
+    res=multbyalpha(res,11)
+
+#representation avec une liste
+def sum(a,b):
+    return list(map(xor, a,b))
+b=[1,1,1,0,1]
+y=[0,1,1,1,1]
+print(sum(b,y))
+
+recompose_nbr = lambda x : reduce(lambda a,b: 2*a+b, x)
+def decompose_nbr(x):
+    res=[]
+    while x>0:
+        res.append(x&1)
+        x>>=1
+    res.reverse()
+    return res
